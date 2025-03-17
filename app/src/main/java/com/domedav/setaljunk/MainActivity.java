6405da5152b1aa8a610d128245d5759abb304a39
domedav <domedav14@gmail.com>
2025-03-18 00:53:25 +0100

    Navigation, some stats UI design

--- app/src/main/java/com/domedav/setaljunk/MainActivity.java
+++ app/src/main/java/com/domedav/setaljunk/MainActivity.java
@@ -1,6 +1,7 @@
 package com.domedav.setaljunk;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Criteria;
@@ -8,7 +9,10 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
@@ -16,10 +20,14 @@ import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatSeekBar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+
+import com.domedav.setaljunk.location.LocationGeneration;
 import com.domedav.setaljunk.permissions.AppPermissions;
 import com.domedav.setaljunk.sharedpreferences.AppDataStore;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -32,6 +40,9 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.textview.MaterialTextView;
 
 import java.util.Objects;
 
@@ -48,6 +59,59 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	private int _currentMeters = MAP_MIN_DISTANCE;
 	
 	private AppCompatSeekBar _mapSeekbar;
+	
+	private AppCompatImageButton _statsButton;
+	private AppCompatImageButton _qrButton;
+	private AppCompatImageButton _walkStartButton;
+	
+	private CircularProgressIndicator _progressbar;
+	private MaterialTextView _helperTextView;
+	
+	private Circle _drawnCircle;
+	private Location _lastLocation;
+	private LatLng _lastLatLng;
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if(_mapView != null)
+			_mapView.onResume();
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if(_mapView != null)
+			_mapView.onStart();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if(_mapView != null)
+			_mapView.onStop();
+	}
+	
+	@Override
+	protected void onPause() {
+		if(_mapView != null)
+			_mapView.onPause();
+		super.onPause();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if(_mapView != null)
+			_mapView.onDestroy();
+		super.onDestroy();
+	}
+	
+	@Override
+	public void onLowMemory() {
+		super.onLowMemory();
+		if(_mapView != null)
+			_mapView.onLowMemory();
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		}
 		
 		// Use recently used value
-		_currentMeters = AppDataStore.getData(AppDataStore.MainPrefsKeys.STOREKEY, AppDataStore.MainPrefsKeys.DATAKEY_RECENT_MAP_DISTANCE, MAP_MIN_DISTANCE);
+		_currentMeters = AppDataStore.getData(AppDataStore.MainPrefsKeys.STOREKEY, AppDataStore.MainPrefsKeys.DATAKEY_RECENT_MAP_DISTANCE, 500); // default walk distance is 500m
 		
 		_mapView = new MapView(getApplicationContext()); // need to initialize like this, so app doesnt crash, when no location permission
 		_mapView.setFocusable(false);
@@ -99,6 +163,27 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		
 		_mapView.onCreate(mapViewBundle);
 		_mapView.getMapAsync(this);
+		
+		_walkStartButton = findViewById(R.id.startnew_button);
+		_statsButton = findViewById(R.id.chart_button);
+		_qrButton = findViewById(R.id.qrcode_button);
+		
+		_progressbar = findViewById(R.id.startnew_progress);
+		_helperTextView = findViewById(R.id.helper_text_display);
+		
+		_walkStartButton.setOnClickListener(l -> {
+			OnWalkButtonPressed();
+		});
+		
+		_statsButton.setOnClickListener(l -> {
+			Intent intent = new Intent(this, StatsActivity.class);
+			startActivity(intent);
+		});
+		
+		_qrButton.setOnClickListener(l -> {
+			Intent intent = new Intent(this, QRActivity.class);
+			startActivity(intent);
+		});
 	}
 	
 	@RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
@@ -106,14 +191,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	public void onMapReady(@NonNull GoogleMap googleMap) {
 		this._googleMap = googleMap;
 		if (!AppPermissions.hasPermission(this, AppPermissions.LOCATION)) { // no location permission, we cant use the maps
+			// user shouldnt reach main activity, without necessary permissions
 			return;
 		}
 		LocationManager locationManager = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+		if(locationManager == null){
+			return;
+		}
 		var gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 		var internet_enabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 		
 		if(!gps_enabled || !internet_enabled){
 			Toast.makeText(getApplicationContext(), getString(R.string.main_gps_missing_services), Toast.LENGTH_LONG).show(); // alert user, that no gps or internet is available
+			//TODO: make this a dialog
 			return;
 		}
 		MapsInitializer.initialize(this);
@@ -143,10 +233,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 		refreshZooming(true);
 	}
 	
-	private Circle _drawnCircle;
-	private Location _lastLocation;
-	private LatLng _lastLatLng;
-	
 	/// Auto aligns the camera and the camera to match the current preferences
 	@RequiresPermission(allOf = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
 	private void refreshZooming(boolean refreshLocation){
@@ -155,6 +241,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 			
 			_lastLocation = locationManager.getLastKnownLocation(Objects.requireNonNull(locationManager.getBestProvider(new Criteria(), false))); // get last known location
 			_lastLatLng = new LatLng(_lastLocation == null ? 0 : _lastLocation.getLatitude(), _lastLocation == null ? 0 : _lastLocation.getLongitude());
+			
+			if(_drawnCircle != null){
+				_drawnCircle.remove();
+				_drawnCircle = null;
+			}
 		}
 		var cameraPosition = new CameraPosition.Builder()
 				.target(_lastLatLng)
@@ -180,59 +271,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	}
 	
 	@Override
-	protected void onResume() {
-		super.onResume();
-		_mapView.onResume();
-	}
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		_mapView.onStart();
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		_mapView.onStop();
-	}
-	
-	@Override
-	protected void onPause() {
-		_mapView.onPause();
-		super.onPause();
-	}
-	
-	@Override
-	protected void onDestroy() {
-		_mapView.onDestroy();
-		super.onDestroy();
-	}
-	
-	@Override
-	public void onLowMemory() {
-		super.onLowMemory();
-		_mapView.onLowMemory();
-	}
-	
-	@Override
 	protected void onSaveInstanceState(@NonNull Bundle outState) {
 		super.onSaveInstanceState(outState);
 		
-		Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
-		if (mapViewBundle == null) {
-			mapViewBundle = new Bundle();
-			outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+		if(_mapView != null){
+			Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+			if (mapViewBundle == null) {
+				mapViewBundle = new Bundle();
+				outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+			}
+			
+			_mapView.onSaveInstanceState(mapViewBundle);
 		}
-		
-		_mapView.onSaveInstanceState(mapViewBundle);
-	}
-	
-	private int getZoomProgress(int meters){
-		var from = MainActivity.MAP_MIN_DISTANCE;
-		var to = MainActivity.MAP_MAX_DISTANCE;
-		// simple lerp
-		return from + (to - from) * (meters / to);
 	}
 	
 	private float getGoogleMapsZoomLevel(double latitude) {
@@ -263,5 +313,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 	public void onStopTrackingTouch(SeekBar seekBar) {
 		AppDataStore.setData(AppDataStore.MainPrefsKeys.STOREKEY, AppDataStore.MainPrefsKeys.DATAKEY_RECENT_MAP_DISTANCE, _currentMeters);
 		refreshZooming(true);
+	}
+	
+	private void OnWalkButtonPressed(){
+		if(_googleMap == null){
+			return;
+		}
+		_walkStartButton.setVisibility(View.INVISIBLE);
+		
+		_helperTextView.setText(getString(R.string.main_usage_generation_in_progress));
+		
+		var animTime = 3500;
+		_progressbar.setVisibility(View.VISIBLE);
+		_progressbar.setProgress(0, false);
+		_progressbar.setMax(100);
+		ObjectAnimator progressAnimator = ObjectAnimator.ofInt(_progressbar, "progress", 0, 100); // animate progressbar
+		progressAnimator.setDuration(animTime);
+		progressAnimator.setInterpolator(new FastOutSlowInInterpolator());
+		progressAnimator.start();
+		
+		var latlng = getRandomLocationInRange();
+		
+		new Handler(Looper.getMainLooper()).postDelayed(() -> {
+			OnGeneratedLocation(latlng);
+		}, animTime); // call after time passed
+	}
+	
+	private void OnGeneratedLocation(LatLng latlng){
+		_progressbar.setVisibility(View.INVISIBLE);
+		_progressbar.setProgress(0, false);
+		_googleMap.addMarker(new MarkerOptions()
+				.position(latlng)
+		);
+	}
+	
+	@NonNull
+	private LatLng getRandomLocationInRange(){
+		return LocationGeneration.generateRandomNearbyLoction(_lastLatLng, _currentMeters, this);
 	}
 }
---
+++ app/src/main/java/com/domedav/setaljunk/location/LocationGeneration.java
@@ -0,0 +1,58 @@
+package com.domedav.setaljunk.location;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.Locale;
+
+public class LocationGeneration {
+	
+	private static final String TAG = "LocationGeneration";
+	
+	/// Generates a random location, by the given parameters
+	@NonNull
+	public static LatLng generateRandomNearbyLoction(@NonNull LatLng currentLocation, double range, Context context){
+		Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+		
+		double randomAngle = Math.random() * 2 * Math.PI; // pick a random direction
+		var randomRadius = Math.sqrt(Math.random()) * range; // in a random radius within the range
+		
+		randomRadius = lerp(randomRadius, range, .65); // add heavy bias towards the end of the radius circle
+		
+		// Convert meters to latitude degrees (approximately)
+		double latOffset = randomRadius * Math.sin(randomAngle) / 111111.0;
+		
+		double lngOffset = randomRadius * Math.cos(randomAngle) / (111111.0 * Math.cos(Math.toRadians(currentLocation.latitude)));
+		
+		// add the result to our current location
+		double newLat = currentLocation.latitude + latOffset;
+		double newLng = currentLocation.longitude + lngOffset;
+		
+		Address address = new Address(Locale.getDefault());
+		address.setLatitude(newLat);
+		address.setLongitude(newLng); // if the address is somehow incorrect, even with geocoder, just use the likely not navigatable generated location
+		try{
+			var addresses = geocoder.getFromLocation(newLat, newLng, 1);
+			if(addresses != null){
+				Log.i(TAG, "generateRandomNearbyLoction: Random Location Name: " + addresses.get(0).toString());
+				address = addresses.get(0);
+			}
+		}
+		catch (IOException iex){
+			Log.e(TAG, "generateRandomNearbyLoction: ", iex);
+		}
+		
+		return new LatLng(address.getLatitude(), address.getLongitude());
+	}
+	
+	private static double lerp(double a, double b, double t){
+		return a + t * (b - a);
+	}
+}
