7c28b2b2bbced4cce858484805926d74aaf35451
domedav <domedav14@gmail.com>
2025-04-14 18:58:09 +0200

    playstore fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.domedav.setaljunk"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0.0"
 
         manifestPlaceholders["apiKey"] = apiKey
